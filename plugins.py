6d73e0cf149534d5d7835e60d6c179a7db4d9bc2
wsg <41861485+wsg96@users.noreply.github.com>
2020-11-18 17:31:21 +0800

    Update plugins.py
    添加阿里云云监控的插件

--- plugins.py
+++ plugins.py
@@ -11,6 +11,7 @@ pluginsVolume = "/opt/plugins"
 
 class ZipFileWithPermissions(zipfile.ZipFile):
     """ Custom ZipFile class handling file permissions. """
+
     def _extract_member(self, member, targetpath, pwd):
         if not isinstance(member, zipfile.ZipInfo):
             member = self.getinfo(member)
@@ -22,11 +23,12 @@ class ZipFileWithPermissions(zipfile.ZipFile):
             os.chmod(targetpath, attr)
         return targetpath
 
+
 def getPlugins():
     result = list()
     if pluginsKey in os.environ and not (not os.environ[pluginsKey]):
         plugins = os.environ[pluginsKey].split(",")
-        for plugin in plugins:        
+        for plugin in plugins:
             parts = plugin.split(":")
             if len(parts) == 2:
                 result.append((parts[0], parts[1]))
@@ -34,17 +36,26 @@ def getPlugins():
                 print("Invalid syntax (version missing?): " + plugin)
     return result
 
+
 def downloadPlugin(plugin):
+    print(plugin)
+    plugin_Key = plugin[0]
     url = "https://grafana.com/api/plugins/%s/versions/%s/download" % plugin
+    if 'aliyun' in plugin_Key:
+        print('下载阿里云的插件')
+        url = "https://github.com/aliyun/%s/archive/master.zip" % plugin_Key
+        print(url)
     file_name = "/tmp/%s_%s.zip" % plugin
     with urllib.request.urlopen(url) as response, open(file_name, "wb") as out_file:
-            shutil.copyfileobj(response, out_file)
+        shutil.copyfileobj(response, out_file)
     return file_name
+
 
 def extractPlugin(file_name):
     zip = ZipFileWithPermissions(file_name)
     zip.extractall(pluginsVolume)
     zip.close()
+
 
 def installPlugin(plugin):
     try:
@@ -54,8 +65,14 @@ def installPlugin(plugin):
     else:
         extractPlugin(file_name)
 
+
 def main():
     for plugin in getPlugins():
         installPlugin(plugin)
 
+
+# 测试案例
+# grafana的官方kubernetes镜像插件和阿里云的云监控插件
+# GRAFANA_PLUGINS = 'grafana-kubernetes-app:1.0.1,aliyun-cms-grafana:latest'
+# os.environ.setdefault('GRAFANA_PLUGINS', GRAFANA_PLUGINS)
 main()
